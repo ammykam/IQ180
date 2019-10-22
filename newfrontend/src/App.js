bf01e3f4524e3635b84d3e4d32e2950b1afebb1a
Natakorn Ammy Kam <na-takorn@hotmail.com>
2019-10-22 14:37:51 +0700

    fix app.js front

--- newfrontend/src/App.js
+++ newfrontend/src/App.js
@@ -8,8 +8,8 @@ import WelcomeDialog from "./components/welcomeDialog";
 import WhosReady from "./components/whosReady";
 import Game from "./components/game";
 
-import Song from "./components/Song";
-import Countdown from "./components/countdown";
+import Song from "./components/song.js";
+import Countdown from "./components/countdown.js";
 
 import socketIOClient from "socket.io-client";
 export const socket = socketIOClient("http://localhost:3000");
